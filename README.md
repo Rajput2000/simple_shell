903bac33759fc41fa41e04a469f1d6c679c3e924
Edeh Sharon <Pearllizzy196@gmail.com>
2023-04-15 11:23:56 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Write a beautiful code that passes the Betty checks
+Write a UNIX command line interpreter.
+Simple shell 0.1 +
+Simple shell 0.2 +
---
+++ shell.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+
+/**
+ * main - code to pass betty
+ * @a - type int
+ * @arg - type char
+ */
+int main(int a, char **arg)
+{
+	return (0);
+}
